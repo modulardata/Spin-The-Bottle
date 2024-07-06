c1b012f31d0db5f41e3dc60f39f79945a5af25d5
Miodrag <3128205+modulardata@users.noreply.github.com>
2024-07-06 11:49:29 +0200

    Stage 3/3
    Simple solutions just to past a test

--- Spin The Bottle/task/src/main.js
+++ Spin The Bottle/task/src/main.js
@@ -1,25 +1,41 @@
-// Array of players
-const players = ["Player 1", "Player 2", "Player 3", "Player 4", "Player 5", "Player 6"];
-// Random player selection
-let randomPlayer = () => players[Math.floor(Math.random() * players.length)]
-// Spin the wheel
-let spinTheWheel = () => {
-    // Spinning animation
-    document.getElementById('status').innerHTML = "Spinning..."
-    // Set a timeout 2 seconds
-    setTimeout(()=>{
-        const chosenPlayer = randomPlayer()
-        document.getElementById('status').innerHTML = `Selected player: ${chosenPlayer}.`
-    }, 2000);
+const players = []
+const addPlayer = (name) => {
+	players.push(name)
+	const playersList = document.getElementById('players')
+	const newListItem = document.createElement('li')
+	newListItem.textContent = name
+	playersList.appendChild(newListItem)
 }
 
-// Event listener for spin button
+document.getElementById('add-player').addEventListener('click', (e) => {
+	e.preventDefault()
+	const name = document.getElementById('player-name').value
+	document.getElementById('status').innerHTML = "Spin the bottle!"
+	if (!name) {
+		alert('Please enter a player name')
+	} else if (players.includes(name)) {
+		// const playersList = document.getElementById('players')
+		document.getElementById('status').innerHTML = "Player already exists!"
+	} else {
+		addPlayer(name)
+		document.getElementById('player-name').value = ''
+		for (const sElement of playersList.children) {
+			sElement.textContent = name
+		}
+	}
+
+})
+
+const randomPlayer = () => {
+	return players[Math.floor(Math.random() * players.length)]
+}
+
+const spinTheWheel = () => {
+	document.getElementById('status').innerHTML = "Spinning..."
+	setTimeout(() => {
+		const chosenPlayer = randomPlayer()
+		document.getElementById('status').innerHTML = `Selected player: ${chosenPlayer}.`
+	}, 2000);
+}
+
 document.getElementById('spin').addEventListener('click', spinTheWheel)
-
-
-
-
-
-
-
-
